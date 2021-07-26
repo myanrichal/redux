3620ee4c9741f1d6755f4422d6b3ef2b7f623503
ryan michal <myanrichal@gmail.com>
2021-07-26 12:38:37 -0400

    dedux working

--- implementing-redux/counter.js
+++ implementing-redux/counter.js
@@ -1,2 +1,63 @@
 import Dedux from './dedux.js'
 const { createStore } = Dedux
+
+
+    const initialState = {
+        count: 0
+    }
+
+    document.getElementById('count').innerHTML = initialState.count
+
+    const reducer = (previousState, action) => {
+        switch (action.type) {
+            case 'INCREMENT':
+                return {
+                    count: previousState.count + 1,
+                };
+            case 'DECREMENT':
+                return {
+                    count: previousState.count - 1,
+                };
+            case 'RESET': 
+                return {
+                    count: previousState.count = 0, 
+                }
+            default:
+                return previousState;
+        }
+    };
+
+    // Creates a dedux store
+    const store = createStore(reducer, initialState); 
+
+    // listen for state change. update HTML when changed
+    const counter = document.getElementById('count');
+    store.subscribe(() => {
+        const state = store.getState();
+        const count = state.count;
+        counter.innerHTML = count;
+    });
+
+    // Dispatch increment
+    document.getElementById('up').addEventListener('click', () => {
+        store.dispatch({
+            type: 'INCREMENT'
+        });
+    });
+    
+    // Dispatch decrement
+    document.getElementById('down').addEventListener('click', () => {
+        store.dispatch({
+            type: 'DECREMENT'
+        });
+    });
+
+    // Dispatch reset
+    document.getElementById('reset').addEventListener('click', () => {
+        store.dispatch({
+            type: 'RESET'
+        });
+    });
+
+    // Run this to initialize the state 
+    store.dispatch({})
--- implementing-redux/dedux.js
+++ implementing-redux/dedux.js
@@ -3,6 +3,39 @@ export default {
   applyMiddleware,
 }
 
-function createStore() {}
+function createStore (reducer, initialState) {
 
-function applyMiddleware() {}
+  const store = {
+    state: initialState,
+    listeners: [], 
+    getState: () => store.state, 
+    subscribe: (listener) => {
+      store.listeners.push(listener); 
+    }, 
+    dispatch: (action) => {
+      // store.state = applyMiddleware(store.state, action); 
+      store.state = reducer(store.state, action); 
+      store.listeners.forEach((listener) => listener()); 
+    }
+  }; 
+  
+  applyMiddleware(store.count); 
+  return store;
+}
+
+function applyMiddleware(previousState, action) {
+
+  return checkLocalStorage(previousState, action); 
+
+}
+
+function checkLocalStorage(previousState, action) {
+  if(localStorage.getItem("count" === null)) {
+    localStorage.setItem("count", count)
+    return previousState
+  }
+  
+  return localStorage.getItem("count"); 
+}
+
+
